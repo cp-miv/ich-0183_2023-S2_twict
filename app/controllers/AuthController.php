b727f5a09aeb2c635c065cba25b8f7b5a2e892da
Michael Vogel <michael.vogel@ceff.ch>
2024-03-03 10:52:48 +0100

    Initial commit for solution

---
+++ app/controllers/AuthController.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controllers;
+
+class AuthController extends \App\Controllers\AppController
+{
+    public function index(): void
+    {
+    }
+}
