696b8ad8a5b27dd435a4c7aa42e9aa9cc38fb37b
Michael Vogel <michael.vogel@ceff.ch>
2024-03-03 10:52:48 +0100

    Initial commit for solution

---
+++ app/controllers/AuthBasicController.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controllers;
+
+use App\Models\User;
+
+class AuthBasicController extends \App\Controllers\AppController
+{
+    private const REALM = 'oPh?\dRG>B413a;E:5';
+
+    public function login(): void
+    {
+        if (empty($_SERVER['PHP_AUTH_USER']) || empty($_SERVER['PHP_AUTH_PW'])) {
+            header('WWW-Authenticate: Basic realm="' . self::REALM . '"');
+            header('HTTP/1.1 401 Unauthorized', true, 401);
+            return;
+        }
+
+
+        $user = User::findByMailAddressAndPassword($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']);
+
+        if ($user == null) {
+            $this->flash->danger('Le nom d\'utilisateur est invalide');
+            $this->redirect('/auth');
+        }
+
+
+        $_SESSION['user'] = $user;
+        $this->flash->success('Le processus de connexion a réussi');
+        $this->redirect('/auth');
+    }
+
+    public function loginCancel(): void
+    {
+        $this->flash->warning('Le processus de connexion a été annulé');
+        $this->redirect('/auth');
+    }
+
+    public function logout(): void
+    {
+        session_destroy();
+        session_start();
+
+        header('WWW-Authenticate: Basic realm="' . self::REALM . '"');
+        header('HTTP/1.1 401 Unauthorized', true, 401);
+
+        $this->flash->success('Le processus de déconnexion a réussi');
+    }
+
+    public function logoutCancel(): void
+    {
+        $this->flash->success('Le processus de déconnexion a réussi');
+        $this->redirect('/auth');
+    }
+}
--- app/models/User.php
+++ app/models/User.php
@@ -86,4 +86,21 @@ class User extends \Core\Model
 
         return $success;
     }
+
+    public static function findByMailAddressAndPassword(string $mailAddress, string $password): array
+    {
+        $db = static::getDB();
+
+        $model = $db
+            ->query(<<< SQL
+                SELECT `id`, `firstname`, `lastname`, `mailAddress`, `password`, `createdAt`, `updatedAt`
+                FROM `users`
+                WHERE `mailAddress`= '{$mailAddress}'
+                AND `password`= '{$password}'
+                LIMIT 1;
+                SQL)
+            ->fetch();
+
+        return $model;
+    }
 }
